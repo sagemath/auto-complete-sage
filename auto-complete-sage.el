51a40542542429c3e3227a95f3ae2b9676748f67
Sho Takemori <stakemorii@gmail.com>
2014-09-13 23:21:43 +0900

    Deleted created.

--- auto-complete-sage.el
+++ auto-complete-sage.el
@@ -4,7 +4,6 @@
 ;; Author: Sho Takemori <stakemorii@gmail.com>
 ;; URL: https://github.com/stakemori/auto-complete-sage
 ;; Keywords: Sage, math, auto-complete
-;; Created: 2012
 ;; Version: 0.0.1
 ;; Package-Requires: ((auto-complete "1.4.0") (sage-shell-mode "0.0.1"))
 
