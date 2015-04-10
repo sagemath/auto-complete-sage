f4d8f83ee4a62af9bd5b1c818ac5d449c65c452d
Sho Takemori <stakemorii@gmail.com>
2015-04-11 04:39:36 +0900

    Show eldoc after invoking ac-complete.

--- auto-complete-sage.el
+++ auto-complete-sage.el
@@ -33,6 +33,8 @@
 (setq sage-shell:completion-function 'auto-complete)
 (add-to-list 'ac-modes 'sage-shell-mode)
 (add-to-list 'ac-modes 'sage-shell:sage-mode)
+(when (fboundp #'eldoc-add-command)
+  (eldoc-add-command #'ac-complete))
 
 (defgroup auto-complete-sage nil "Group for auto-compete-sage"
   :group 'sage-shell)
